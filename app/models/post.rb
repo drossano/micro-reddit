043e0975248f86d9b5e8ec6f6dcdd76011bfe20e
Dean <72749793+drossano@users.noreply.github.com>
2023-03-30 12:50:58 -0400

    Add post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { in: 1..100 }
+  validates :body, presence: true, length: {minimum: 1}
+  validates :user_id, presence: true
 end
