63fc7b118a84b795e026248f0f7abb45e6b9ae73
Dean <72749793+drossano@users.noreply.github.com>
2023-03-30 22:11:54 -0400

    Add uniqueness validation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username, presence: true, length: { in: 1..20 }
+  validates :username, presence: true, length: { in: 1..20 }, uniqueness: true
 
   has_many :posts
   has_many :comments
